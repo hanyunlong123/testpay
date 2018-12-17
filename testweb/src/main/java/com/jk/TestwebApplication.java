8f3941f576d2bc35c485d2005ad108f5aaf9f8a9
hanyl <175751974@qq.com>
2018-12-17 16:55:08 +0800

    主线更

--- testweb/src/main/java/com/jk/TestwebApplication.java
+++ testweb/src/main/java/com/jk/TestwebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestwebApplication {
 
     public static void main(String[] args) {
-        System.out.println("234");
+        System.out.println("123");
         SpringApplication.run(TestwebApplication.class, args);
     }
 
